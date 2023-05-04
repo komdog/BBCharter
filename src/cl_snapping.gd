ac00fa92d5287c7b02310d6a5e9d01d1e09eccf6
komdog <komdogart@gmail.com>
2023-05-03 23:12:36 -0400

    Snapping Hotkeys closes #6

--- src/cl_snapping.gd
+++ src/cl_snapping.gd
@@ -10,9 +10,23 @@ func _ready():
 func _on_item_selected(index):
 	Global.snapping_factor = Global.snapping_ratios[index]
 	Events.emit_signal('update_snapping', index)
+	selected = index
 
 func _on_update_snapping(index):
 	print('Snapping set to 1/%s' % Global.snapping_ratios[index])
+
+func _input(event):
+	
+	if Popups.open: return
+	
+	if event.is_action_pressed("snap_1"):
+		_on_item_selected(0)
+	elif event.is_action_pressed("snap_2"):
+		_on_item_selected(1)
+	elif event.is_action_pressed("snap_3"):
+		_on_item_selected(2)
+	elif event.is_action_pressed("snap_4"):
+		_on_item_selected(3)
 
 
 func _on_snap_toggle_toggled(state: bool):
