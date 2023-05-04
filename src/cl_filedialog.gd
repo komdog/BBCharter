726a3387d292951c0cde7942b2a0fb3bc9e4a786
komdog <komdogart@gmail.com>
2023-05-03 23:08:03 -0400

    Root subfolder stuff

--- src/cl_filedialog.gd
+++ src/cl_filedialog.gd
@@ -9,7 +9,7 @@ func open_project_dialog():
 	print('Opening New Project...')
 	
 	# DELETE THIS ON RELEASE
-	Global.filedialog.current_dir = "D:/Mega/Projects/BunFan/Games/beat_banger/_project/data/act1/"
+	Global.filedialog.current_dir = "D:/Mega/Projects/BunFan/Games/BeatBanger/_project/mods/example_modpack"
 	
 	Global.filedialog.title = "Select Project Folder"
 	Global.filedialog.file_mode = FileDialog.FILE_MODE_OPEN_DIR
