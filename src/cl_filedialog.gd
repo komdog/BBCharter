1daf5874b3dd905da44f3d8f78afad03e0c6c21c
komdog <komdogart@gmail.com>
2023-05-04 21:03:10 -0400

    Added debug open dir path

--- src/cl_filedialog.gd
+++ src/cl_filedialog.gd
@@ -9,7 +9,8 @@ func open_project_dialog():
 	print('Opening New Project...')
 	
 	# DELETE THIS ON RELEASE
-	Global.filedialog.current_dir = "D:/Mega/Projects/BunFan/Games/BeatBanger/_project/mods/example_modpack"
+	if OS.is_debug_build():
+		Global.filedialog.current_dir = ProjectSettings.get_setting("global/debug_open_dir")
 	
 	Global.filedialog.title = "Select Project Folder"
 	Global.filedialog.file_mode = FileDialog.FILE_MODE_OPEN_DIR
